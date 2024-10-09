efa60665348dd43f866f2152d41d7c9815e95c98
agadin <a.gadin@wustl.edu>
2024-10-09 10:59:24 -0500

    reworked pin calling

--- call_back_test.py
+++ call_back_test.py
@@ -1,21 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO pin number
-HALL_SENSOR_PIN = 17  # Change to the correct GPIO pin
+# Configure GPIO
+HALL_SENSOR_PIN = 17  # GPIO pin number
 
-# Set up GPIO
+# Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin as input with pull-down resistor
 
+
+# Callback function to handle Hall effect sensor state changes
+def hall_effect_callback(channel):
+    if GPIO.input(HALL_SENSOR_PIN):
+        print("Magnetic field detected!")  # Triggered when the magnetic field is detected
+    else:
+        print("Magnetic field lost!")  # Triggered when the magnetic field is no longer detected
+
+
+# Add event detection for the Hall effect sensor
 try:
+    GPIO.add_event_detect(HALL_SENSOR_PIN, GPIO.BOTH, callback=hall_effect_callback, bouncetime=200)
+    print("Monitoring Hall effect sensor. Press Ctrl+C to exit.")
+
+    # Keep the script running to listen for events
     while True:
-        state = GPIO.input(HALL_SENSOR_PIN)
-        if state:
-            print("Magnetic field detected!")
-        else:
-            print("Magnetic field lost!")
-        time.sleep(1)  # Sleep for 1 second
+        time.sleep(1)  # Wait indefinitely
+
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
