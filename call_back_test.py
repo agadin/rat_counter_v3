fdc7d065fb965a2c366f1dea4011eb4cf6bc6334
agadin <a.gadin@wustl.edu>
2024-10-09 11:00:34 -0500

    reworked pin calling

--- call_back_test.py
+++ call_back_test.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import threading
 import time
 
 # Configure GPIO
@@ -6,25 +7,30 @@ HALL_SENSOR_PIN = 17  # GPIO pin number
 
 # Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin as input with pull-down resistor
+GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-
-# Callback function to handle Hall effect sensor state changes
+# Callback function
 def hall_effect_callback(channel):
     if GPIO.input(HALL_SENSOR_PIN):
-        print("Magnetic field detected!")  # Triggered when the magnetic field is detected
+        print("Magnetic field detected!")
     else:
-        print("Magnetic field lost!")  # Triggered when the magnetic field is no longer detected
+        print("Magnetic field lost!")
 
-
-# Add event detection for the Hall effect sensor
-try:
+# Thread function to monitor the Hall effect sensor
+def monitor_sensor():
     GPIO.add_event_detect(HALL_SENSOR_PIN, GPIO.BOTH, callback=hall_effect_callback, bouncetime=200)
-    print("Monitoring Hall effect sensor. Press Ctrl+C to exit.")
-
-    # Keep the script running to listen for events
     while True:
-        time.sleep(1)  # Wait indefinitely
+        time.sleep(1)  # Keep the thread alive
+
+# Start the monitoring in a separate thread
+sensor_thread = threading.Thread(target=monitor_sensor)
+sensor_thread.daemon = True  # Daemonize thread to exit when the main program exits
+sensor_thread.start()
+
+try:
+    print("Monitoring Hall effect sensor in a separate thread. Press Ctrl+C to exit.")
+    while True:
+        time.sleep(1)  # Keep the main program running
 
 except KeyboardInterrupt:
     print("Exiting...")
