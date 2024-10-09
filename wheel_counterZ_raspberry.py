f3447eba3b351d4d9b6c29ff912b55cb666cd220
agadin <a.gadin@wustl.edu>
2024-10-09 10:21:45 -0500

    reworked pin calling

--- wheel_counterZ_raspberry.py
+++ wheel_counterZ_raspberry.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the virtual environment's site-packages to the PYTHONPATH
-venv_path = "/Documents/rat_counter_v3/myvenv/lib/python3.x/site-packages"
+venv_path = "/home/lakelab/Documents/rat_counter_v3/myvenv/lib/python3.x/site-packages"
 sys.path.append(venv_path)
 
 import RPi.GPIO as GPIO
